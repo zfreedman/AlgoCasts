8997c5142a061dfae11ff450a028ab10ab601cf5
zach <zfreedman92@gmail.com>
2019-04-04 22:37:38 -0700

    feat: implemented solution for anagrams Please enter the commit message for your changes. Lines starting

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,41 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams1(stringA, stringB) {
+  const sortedString = string => string.toLowerCase().replace(/[.\-?! ]/g, '').split('').sort().join('')
+  return sortedString(stringA) === sortedString(stringB)
+}
 
-module.exports = anagrams;
+function anagrams2(a, b) {
+  // first create hash tables for a and b's letters (lowercase)
+  let counterA = makeHashFromstring(a)
+  let counterB = makeHashFromstring(b)
+
+  // compare the kvps of each hash table. if the sizes of the HT are different
+  // OR if a single value mismatches OR if 1 key is present in one hash but
+  // not the other, we knowthese 2 strings are not anagrams
+  
+  // size mismatch
+  if (Object.keys(counterA).length !== Object.keys(counterB).length) {
+    return false
+  }
+
+  // key-by-key comparison
+  for (let ch in counterA) {
+    if (!(ch in counterB) || counterA[ch] !== counterB[ch]) return false
+  }
+
+  return true
+}
+const makeHashFromstring = string => {
+  const counter = {}
+  for (let ch of string) {
+    ch = ch.toLowerCase()
+    if (!'.?!- '.includes(ch)) {
+      ch in counter ? counter[ch]++ : counter[ch] = 0
+    }
+  }
+  return counter
+}
+
+module.exports = anagrams2;
