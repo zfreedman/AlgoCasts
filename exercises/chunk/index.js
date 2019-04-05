2c2ddffa8aa4c067a1078a7c575f0957fc592c95
zach <zfreedman92@gmail.com>
2019-04-04 22:09:08 -0700

    feat; solution for array chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,28 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // arr = [1,2,3,4]
+  // size = 2
+  // result = [[1, 2], [3, 4]]
+  const result = []
+  for (let i = 0; i < array.length; i++) {
+    if (i % size === 0) {
+      result.push([])
+    }
+    result[result.length - 1].push(array[i])
+  }
+  return result
+}
 
-module.exports = chunk;
+function chunk1(array, size) {
+  return array.reduce((acc, curr, i) => {
+    // add on a new subarray when the current one is filled
+    if (i % size === 0) acc.push([])
+    // always elements onto the last sub array
+    acc[acc.length - 1].push(curr)
+    return acc
+  }, [])
+}
+
+module.exports = chunk1;
