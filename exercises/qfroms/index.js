d6e61402f0781f1d189660965ec968f2a55bd763
zach <zfreedman92@gmail.com>
2019-04-09 22:21:02 -0700

    feat: solved queue from stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,30 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.inOrder = new Stack()
+    this.reverse = new Stack()
+  }
+
+  add(e) {
+    while (this.inOrder.peek() !== undefined) {
+      this.reverse.push(this.inOrder.pop())
+    }
+
+    this.inOrder.push(e)
+    while (this.reverse.peek() !== undefined) {
+      this.inOrder.push(this.reverse.pop())
+    }
+  }
+
+  remove() {
+    return this.inOrder.pop()
+  }
+  
+  peek() {
+    return this.inOrder.peek()
+  }
+}
 
 module.exports = Queue;
--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,14 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.data = []
+  }
+  peek() { return this.data[this.data.length - 1] }
+  pop() { return this.data.pop() }
+  
+  push(e) { this.data.push(e) }
+}
 
 module.exports = Stack;
