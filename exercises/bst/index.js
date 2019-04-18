ed10faf97397a2d429457461e8d4123776ea7c74
zach <zfreedman92@gmail.com>
2019-04-18 07:14:46 -0700

    feat: solutions for BST contains and insert

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,30 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+
+  contains(data) {
+    if (data === this.data) return this
+    else if (this.left !== null && data < this.data) return this.left.contains(data)
+    else if (this.right !== null && data > this.data) return this.right.contains(data)
+    return null
+  }
+
+  insert(data) {
+    if (data < this.data) {
+      if (this.left !== null) this.left.insert(data)
+      else this.left = new Node(data)
+    }
+    else if (data > this.data) {
+      if (this.right !== null) this.right.insert(data)
+      else this.right = new Node(data)
+    }
+  }
+}
 
 module.exports = Node;
